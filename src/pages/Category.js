a3d1dcd2c6c19fb5c21996082b8314b40f305519
Yogi1305 <yogesh.22273@knit.ac.in>
2024-09-16 11:26:43 +0530

    title change

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,8 +1,6 @@
 import {useEffect, useState} from "react";
 
 import {useParams} from "react-router-dom";
-
-
 import { GifState } from "../context/context";
 import Gif from "../component/Gif";
 import FollowOn from "../component/follow-on";
